fe5be4b32a6dccfe00af150581014da527cfd362
mushfiqur rahman <mushfiqurrahman.mr1@gmail.com>
2024-04-30 21:06:09 +0600

    trying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([])
+  // useEffect( ()=>{}, [])
+  // const myfunc = () => {}
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border:'2px solid red', margin:'10px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(33); //useState(33, f) if want to wact you can console log it example : const abc = useState(33) next line console.log(abc);
+
+  const increaseCount = () => setCount(count+1);
+  const decreaseCount = () => setCount(count-1);
+
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+/*
+    const products = [
+    {name:'Laptop', price: 150000},
+    {name:'Phone', price: 95000},
+    {name:'Watch', price: 33000},
+    {name:'Tablet', price: 50000}
+  ]
+*/
+/*
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
+      <Product name="Laptop" price="47000"></Product>
+      <Product name="Phone" price="75000"></Product>
+      <Product name="Watch" price="3500"></Product>
+*/
+
+function Product(props){
+  return(
+    <div className='product'>
+       <h3>Name:{props.name}</h3>
+       <p>Price:{props.price}</p>
+    </div>
+  )
 }
 
 export default App;
